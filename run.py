7edd4103abe901239936446619a465d1f16d0fd8
Wayne Halloran <whalloranirl@protonmail.com>
2024-03-14 21:59:29 +0000

    Updated function to add grid from a selected size

--- run.py
+++ run.py
@@ -1,2 +1,11 @@
 #importing the random number generator
 import random
+
+# Function to create a grid from user input
+def create_grid(size):
+    grid = []
+    for _ in range(size):
+        row = ['O'] * size
+        grid.append(row)
+    return grid
+
