dc8405225f408a38cbe1298cafd576edebfa141c
Wayne Halloran <whalloranirl@protonmail.com>
2024-03-14 22:04:58 +0000

    Added function to play game

--- run.py
+++ run.py
@@ -22,9 +22,50 @@ def place_ships(grid, num_ships):
     for _ in range(num_ships):
         x = random.randint(0, size - 1)
         y = random.randint(0, size - 1)
-        
+
         # Ensure ships are not placed on top of each other
         while grid[x][y] == 'X':
             x = random.randint(0, size - 1)
             y = random.randint(0, size - 1)
         grid[x][y] = 'X'
+
+
+# Function to check if a guess is valid and inside the grid boundries
+def is_valid_guess(guess, size):
+    x, y = guess
+    return 0 <= x < size and 0 <= y < size
+
+# Function to play the game
+def play_game(size, num_ships):
+    print("Welcome to Battleship!")
+    print("Try to sink the computer's ships.")
+    print("Enter row and column numbers to make a guess.")
+
+    grid = create_grid(size)
+    place_ships(grid, num_ships)
+    print_grid(grid)
+
+    while True:
+        guess_str = input("Enter your guess (row column): ")
+        try:
+            guess = tuple(map(int, guess_str.split()))
+            if not is_valid_guess(guess, size):
+                print("Your guess is off-grid. Try again.")
+                continue
+        except ValueError:
+            print("Invalid input. Please enter row and column numbers.")
+            continue
+
+        x, y = guess
+        if grid[x][y] == 'X':
+            print("Congratulations! You sank a battleship!")
+            grid[x][y] = '!'
+            print_grid(grid)
+            if all('X' not in row for row in grid):
+                print("Congratulations! You won!")
+                break
+        else:
+            print("You missed!")
+            grid[x][y] = '-'
+            print_grid(grid)
+
